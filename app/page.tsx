1697dc9aa039cc78096f9da26bbc60e95831a424
sohancwact <sohancwactfw@gmail.com>
2023-10-12 13:56:47 +0545

    adding avatar icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
+"use client";
 import { ThemeSwitcher } from "@/utils/ThemeSwitcher";
 import { Workflow } from "lucide-react";
-import Image from "next/image";
-
+import { Avatar } from "@nextui-org/react";
 export default function Home() {
   return (
-    <main>
+    <main className="m-2">
       <ThemeSwitcher />
-      <div className="div_content  h-40 w-60 rounded-md shadow-md m-2">
-        <div className="icon backdrop-blur-md bg-white/30 w-10 p-2 rounded-md ">
+      <div className="div_conten flex justify-between">
+        <div className="icon bg-red-500 shadow-md dark:backdrop-blur-md dark:bg-red-500 p-2 rounded-md max-h-10  ">
           <Workflow />
         </div>
-
-        <span className="text-gray-400 text-sm"> Hello Sohan,</span>
-        <h2 className="font-semibold text-2xl">You've got 8 tasks today</h2>
+        <div className="profile flex justify-center gap-2 ">
+          <Avatar
+            src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
+            size="md"
+          />
+          <h3 className="text-center  font-bold text-sm flex flex-col  pt-1">
+            Sohan cwact
+            <span className="text-gray-400 text-[12px] font-semibold mt-[-4px]">
+              web developer
+            </span>
+          </h3>
+        </div>
       </div>
+      <span className="text-gray-400 text-sm"> Hello Sohan,</span>
+      <h2 className="font-semibold text-2xl">You've got 8 tasks today</h2>
     </main>
   );
 }
--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      {" "}
       <NextThemesProvider attribute="class" defaultTheme="dark">
         {children}
       </NextThemesProvider>
